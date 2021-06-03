9a61756987ebd5b29bc47298ea161a8b7ea104be
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-06-02 21:03:59 -0400

    comment

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,27 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react"; // this is allowing us to use state variables in our functional component
 
-const AddTodo = ({ onAdd }) => { 
-    const [item, setItem] = useState('')
+const AddTodo = ({ onAdd }) => {
+  // functional component creation
+  const [item, setItem] = useState(""); // setting the state of this component saying hey there is going to be an item state and setItem state(i think this is what is inside of an input)
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        onAdd({item})
+  const onSubmit = (e) => {
+    // defining an on submit function to run on our input
+    e.preventDefault(); //says if the event doesnt go as planned dont do what u usually would
+    onAdd({ item }); // this is what the above function takes in
 
-        setItem('')
-    }
+    setItem(""); // clears the input when the item is added
+  };
 
-    return (
-        <form onSubmit={onSubmit}>
-            <label>Add Todo:</label>
-            <input 
-                type="text" 
-                placeholder="Todo Item"
-                value={item}
-                onChange={ (e) => setItem(e.target.value)}
-            /> 
-            <input type="submit" value="Add Todo" />
-        </form>
-    )
-}
+  return (
+    // this is the form that is returned. we will plug it into the dom in src/App.js
+    <form onSubmit={onSubmit}>
+      {" "}
+      {/*event listener which handles state is called on submit and it runs the code from line 7 */}
+      <label>Add Todo:</label>
+      <input
+        type="text"
+        placeholder="Todo Item"
+        value={item}
+        onChange={(e) => setItem(e.target.value)} //onChange event--> setItem(line 10) takes in the value of the input
+      />
+      <input type="submit" value="Add Todo" />
+    </form>
+  );
+};
 
-export default AddTodo
+export default AddTodo; //sends it to be used outside of here
